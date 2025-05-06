b2223051280e6312485cbb38f6bcc1bce804e9ce
aman kumar <aman163@gmail.com>
2025-05-07 03:33:11 +0530

    code formating

--- models/job.js
+++ models/job.js
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
 
 const JobSchema = new mongoose.Schema({
-    company:{
-        type:String,
-        required:[true,"Please provide company!!"],
-        maxlength:50
+    company: {
+        type: String,
+        required: [true, "Please provide company!!"],
+        maxlength: 50
     },
-    position:{
-        type:String,
-        required:[true,"Please provide position!!"],
-        maxlength:50
+    position: {
+        type: String,
+        required: [true, "Please provide position!!"],
+        maxlength: 50
     },
-    status:{
-        type:String,
-        enum:['interview','declined','pending'],
-        default:'pending',
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending',
     },
-    createdBy:{
-        type:mongoose.Types.ObjectId,
-        ref:'User',
-        required:[true,'Please provide user']
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user']
     }
-},{timestamps:true})
+}, { timestamps: true })
 
-export default mongoose.model('Job',JobSchema);
+export default mongoose.model('Job', JobSchema);
